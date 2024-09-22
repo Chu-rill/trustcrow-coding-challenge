57f6a4c356fe82e44096f22c5921e61d87968e17
Chu-rill <churchillrimamskep@gmail.com>
2024-09-22 14:10:44 +0100

    edited the readme

--- index.js
+++ index.js
@@ -18,16 +18,19 @@ const port = process.env.PORT || 3000; // Provide a fallback port in case PORT i
 const limiter = rateLimit({
   max: 100, // 100 requests
   windowMs: 60 * 60 * 1000, // 1 hour
-  message: "We have received too many requests from this IP. Please try again after one hour.",
+  message:
+    "We have received too many requests from this IP. Please try again after one hour.",
 });
 
 // Middleware setup
-app.use(cors({
-  origin: process.env.ALLOWED_URL || "*", // Your frontend URL, default to *
-  methods: "*",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.ALLOWED_URL || "*", // Your frontend URL, default to *
+    methods: "*",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(limiter);
